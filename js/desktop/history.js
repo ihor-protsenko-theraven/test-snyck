b265b5a89602f451c2ee3acd5fb4ccdaef19f662
ihor-protsenko-theraven <ihor.protsenko@theraven.tech>
2022-01-13 14:01:04 +0200

    load-content3

--- js/desktop/history.js
+++ js/desktop/history.js
@@ -209,7 +209,7 @@ $(function() {
     
         $.get(url, function(data){
             $('#tab-event-detail').trigger('click');
-            $('#load-content').html(data).text();
+            $('#load-content').text(data);
             DOMPurify.sanitize($('#tab-event-log').attr('href', $('#tab-event-log').attr('href').replace(/\d+$/, $this.attr('id'))));
             
             if ($('#tab-take-action').attr('href')!=null){
